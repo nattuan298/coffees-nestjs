d761066cde84e3391711c2eda785114e4d16b495
nattuan298 <ntuan2908@gmail.com>
2021-07-21 16:03:15 +0700

    first commit

---
+++ src/coffee/coffee.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeeService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Cold Brew',
+      brand: 'High Land',
+      flavors: ['Choco', 'Vano'],
+    },
+  ];
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
+  }
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      //Update something
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
